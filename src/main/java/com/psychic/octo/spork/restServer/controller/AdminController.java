f845c7aca7202402ce69ccc88d9de7b8668dc9b6
jedoGit <ajvillardo@gmail.com>
2025-05-13 18:49:06 -0400

    Updated the controller endpoints

--- src/main/java/com/psychic/octo/spork/restServer/controller/AdminController.java
+++ src/main/java/com/psychic/octo/spork/restServer/controller/AdminController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/admin")
+@RequestMapping("/api/admin")
 //@PreAuthorize("hasRole('ROLE_ADMIN')") // Enabled in the defaultSecurityFilterChain() using requestMatchers
 public class AdminController {
 
--- src/main/java/com/psychic/octo/spork/restServer/controller/CsrfController.java
+++ src/main/java/com/psychic/octo/spork/restServer/controller/CsrfController.java
@@ -11,7 +11,7 @@ public class CsrfController {
     // This only returns the csrf token from the client which was received from the HTTP request.
     // We'll pass this back to client specifically to be used in POSTMan so we can perform some
     // submission with csrf security enabled in the backend.
-    @GetMapping("/api/v1/csrf-token")
+    @GetMapping("/api/csrf-token")
     public CsrfToken csrfToken(HttpServletRequest request) {
         return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
     }
